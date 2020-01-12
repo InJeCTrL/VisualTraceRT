55565d4d63c40840e77174f3df20692bbb3c82a8
InJeCTrL <dap1933@hotmail.com>
2020-01-12 19:11:10 +0800

    基本功能实现

---
+++ VisualTraceRT/Hop.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisualTraceRT
+{
+    /// <summary>
+    /// 单跳数据
+    /// </summary>
+    public class Hop
+    {
+        /// <summary>
+        /// 跳数
+        /// </summary>
+        public string Distance { get; set; }
+        /// <summary>
+        /// IP地址
+        /// </summary>
+        public string IP { get; set; }
+        /// <summary>
+        /// 地理信息
+        /// </summary>
+        public string Geo { get; set; }
+        /// <summary>
+        /// 自治系统
+        /// </summary>
+        public string AS { get; set; }
+        /// <summary>
+        /// ISP信息
+        /// </summary>
+        public string ISP { get; set; }
+        /// <summary>
+        /// 来源
+        /// </summary>
+        public string Origin { get; set; }
+        /// <summary>
+        /// 时延
+        /// </summary>
+        public string Delay { get; set; }
+        /// <summary>
+        /// 纬度
+        /// </summary>
+        public string Lat { get; set; }
+        /// <summary>
+        /// 经度
+        /// </summary>
+        public string Lon { get; set; }
+    }
+}
---
+++ VisualTraceRT/MainWindow.xaml.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Threading;
+using System.Windows;
+
+namespace VisualTraceRT
+{
+    /// <summary>
+    /// 新字符串行委托
+    /// </summary>
+    /// <param name="line">cmd传回的字符串行</param>
+    public delegate void NewCMDLine(string line);
+    /// <summary>
+    /// 命令运行结束委托
+    /// </summary>
+    /// <param name="e"></param>
+    public delegate void NoMoreCMD(EventArgs e);
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public event NewCMDLine ReadCMDLine;
+        public event NoMoreCMD EndCMD;
+        /// <summary>
+        /// 绑定到DataGrid的数据列表
+        /// </summary>
+        private ObservableCollection<Hop> HopList = new ObservableCollection<Hop>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            ReadCMDLine += new NewCMDLine(ReadCMDLineAction);
+            EndCMD += new NoMoreCMD(EndCMDAction);
+        }
+        /// <summary>
+        /// 获取单跳的详细数据
+        /// </summary>
+        /// <param name="node">单跳节点</param>
+        private void SetHopDetails(Hop node)
+        {
+            WebRequest request= WebRequest.Create("http://ip-api.com/line/" + node.IP + "?lang=zh-CN");
+            WebResponse response = request.GetResponse();
+            Stream s = response.GetResponseStream();
+            StreamReader sr = new StreamReader(s);
+            // IP地址数据是否有效
+            string str_status = sr.ReadLine();
+            if (str_status.Equals("success") == true)
+            {
+                // 设置地理位置
+                node.Geo = sr.ReadLine();
+                // 国家编号
+                sr.ReadLine();
+                // 区域编号
+                sr.ReadLine();
+                // 设置地理位置(区域名称)
+                node.Geo += " " + sr.ReadLine();
+                // 设置地理位置(城市名称)
+                node.Geo += " " + sr.ReadLine();
+                sr.ReadLine();
+                // 设置纬度
+                node.Lat = sr.ReadLine();
+                // 设置经度
+                node.Lon = sr.ReadLine();
+                // 时区
+                sr.ReadLine();
+                // 设置互联网服务提供商
+                node.ISP = sr.ReadLine();
+                // 设置来源
+                node.Origin = sr.ReadLine();
+                // 设置自治系统
+                node.AS = sr.ReadLine();
+            }
+            sr.Dispose();
+            sr.Close();
+            s.Dispose();
+            s.Close();
+        }
+        /// <summary>
+        /// 命令行调用tracert
+        /// </summary>
+        /// <param name="IP"></param>
+        private void TraceByCMD(string IP)
+        {
+            Process p = new Process();
+            p.StartInfo.FileName = "cmd.exe";
+            // 使用windows自带tracert, 超时1s, 最大跃点30
+            p.StartInfo.Arguments = "/c tracert -d -w 1000 -h 30 " + IP;
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.RedirectStandardInput = true;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.CreateNoWindow = true;
+            p.OutputDataReceived += P_OutputDataReceived;
+            p.Exited += P_Exited;
+            p.EnableRaisingEvents = true;
+            p.Start();
+            p.BeginOutputReadLine();
+        }
+        /// <summary>
+        /// 捕获到命令运行结束
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void P_Exited(object sender, EventArgs e)
+        {
+            this.Dispatcher.Invoke(EndCMD, new object[] { e } );
+        }
+        /// <summary>
+        /// 捕获到新的重定向输出数据
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void P_OutputDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            if (e.Data != null)
+            {
+                this.Dispatcher.Invoke(ReadCMDLine, new object[] { e.Data });
+            }
+        }
+        /// <summary>
+        /// 获取新的字符串行动作
+        /// </summary>
+        /// <param name="result"></param>
+        private void ReadCMDLineAction(string line)
+        {
+            // 跳数(距离)
+            int dist = 0;
+            string[] items = line.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            if (items.Length >= 5 && Int32.TryParse(items[0], out dist) == true)
+            {
+                // 时延数量
+                int num_delay = 0;
+                // 实例化新节点
+                Hop newHop = new Hop();
+                newHop.Distance = items[0];
+                int index_items = 1;
+                for (; index_items < items.Length && num_delay < 3; index_items++)
+                {
+                    if (items[index_items].Equals("ms") == false && 
+                        items[index_items].Equals("毫秒") == false)
+                    {
+                        newHop.Delay += items[index_items];
+                        if (++num_delay != 3)
+                        {
+                            newHop.Delay += " / ";
+                        }
+                    }
+                }
+                while (items[index_items].Equals("ms") == true ||
+                        items[index_items].Equals("毫秒") == true)
+                {
+                    ++index_items;
+                }
+                newHop.IP = items[index_items];
+                SetHopDetails(newHop);
+                HopList.Add(newHop);
+                TraceGrid.ItemsSource = HopList;
+            }
+        }
+        /// <summary>
+        /// 命令运行结束后动作
+        /// </summary>
+        /// <param name="e"></param>
+        private void EndCMDAction(EventArgs e)
+        {
+            // 地图页面
+            string str_MapInside = @"<!doctype html>
+                                        <html lang='en'>
+                                          <head>
+                                            <meta charset='utf-8'>
+                                            <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+                                            <meta name='viewport' content='initial-scale=1.0, user-scalable=no, width=device-width'>
+                                            <style>
+                                            html,
+                                            body,
+                                            #container {
+                                              width: 100%;
+                                              height: 100%;
+                                            }
+                                            </style>
+                                          </head>
+                                          <body scroll='no'>
+                                            <div id='container' class='map' tabindex='0'></div>
+                                            <script src='https://webapi.amap.com/maps?v=1.4.15&key=259c97b0e4e74d590717244ccc7d8619'></script>
+                                            <script type='text/javascript'>
+                                            var map = new AMap.Map('container');
+                                            var traceroute = [";
+        foreach (var node in HopList)
+        {
+            if (node.Lat != null &&
+                node.Lon != null)
+            str_MapInside += "[" + node.Lon + ", " + node.Lat + "],";
+        }
+        str_MapInside +=
+                                            @"];
+                                            for (var i = 0; i < traceroute.length; i++) {
+                                                new AMap.Marker({
+                                                    map: map,
+                                                    position: traceroute[i]
+                                                });
+                                            }
+                                            var polyline = new AMap.Polyline({
+                                                path: traceroute,
+                                                showDir: true,
+                                                strokeColor: '#FF0000',
+                                                strokeWeight: 8,
+      	                                        lineJoin: 'round',
+      	                                        lineCap: 'round'
+                                            });
+
+                                            map.add([polyline])
+                                            map.setFitView()
+                                            </script>
+                                        </body>
+                                        </html>
+                                        ";
+            // 显示地图
+            this.traceMap.NavigateToString(str_MapInside);
+            Trace.Content = "TraceRT";
+            Trace.IsEnabled = true;
+        }
+        /// <summary>
+        /// 单击TraceRT按钮
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Trace.IsEnabled = false;
+            Trace.Content = "Tracing...";
+            HopList.Clear();
+            TraceByCMD(targetIP.Text);
+        }
+    }
+}
